4e04acd419231eff3ec2c920fd3e65bac2fb8071
Valeria Meza <valeriameza173@gmail.com>
2020-07-03 12:21:33 -0500

    added TodoItem file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Todos from "./components/Todos";
 
-function App() {
-  return <div className="App">
-    <h1>
-      <Todos />
-    </h1>
-  </div>;
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Clean Room",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Code",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <Todos todos={this.state.todos} />
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
+import TodoItem from "./TodoItem"
 
-function Todos() {
-  return <div >
-    <h1>
-      Todos
-    </h1>
-  </div>;
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => (<TodoItem />));
+  }
 }
 
 export default Todos;
